c04cc747b4775e281775860c699d9014537295c7
Евгений Трофимов <t0ny5t4rk69@yandex.ru>
2023-01-06 19:29:35 +0300

    V2

--- yatup/posts/views.py
+++ yatup/posts/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-# def index() {
-#     return HttpResponse ("главная страница")
-# }
+context = {
+    'index': "Это главная страница проекта Yatube",
+    'group_posts': "Информация о группах проекта Yatube",
+}
 
 def index(request):
-    template = "posts/index.html"
-    return render(request, template)
+    template = "templates/index.html"
+    return render(request, template, context)
 
-def group_posts(): 
-    return HttpResponse ("группы")
+def group_posts(request):
+    template = "templates/group_posts.html" 
+    return render(request, template, context)
